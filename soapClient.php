73bc34f9c3207764260b8d8a5a07215ba9bdba05
IVAM GALVAO FILHO <programador.ivam@gmail.com>
2019-11-11 15:49:43 -0300

    Create Soap.php

---
+++ soapClient.php
@@ -0,0 +1,83 @@
+<?php
+try {
+        // Settings Soap
+        // ======================================================================================================
+        $arrContextOptions=array("ssl"=>array( "verify_peer"=>false, "verify_peer_name"=>false,'crypto_method' => STREAM_CRYPTO_METHOD_TLS_CLIENT));
+       
+        // options for ssl in php 5.6.5
+        $opts = array(
+            'ssl' => array('ciphers'=>'RC4-SHA', 'verify_peer'=>false, 'verify_peer_name'=>false)
+        );
+        // SOAP 1.2 client
+        $params = array ('encoding' => 'UTF-8', 'verifypeer' => false, 'verifyhost' => false, 'soap_version' => SOAP_1_2, 'trace' => 1, 'exceptions' => 1, "connection_timeout" => 180, 'stream_context' => stream_context_create($opts) );
+        // ======================================================================================================
+        // Settings Soap
+       
+        // Create the Auth instance
+        $user        = "06096391000176";
+        $password    = "33BPM@06096391000176";
+        $auth = array(
+             'login' => $user,
+             'password' => $password,
+        );
+
+        // Parâmetros de Entrada
+        $identificador_proprio = "";
+        $ano_calculo = 2019;
+        $data_base_calculo = 26/08/2019;
+        $codigo_sub_receita = 999;
+        $cpfcnpj = "074.633.689-63";
+        $codigo_bloco = 44;
+        $codigo_item = 1;
+        $sequencia_item = 1;
+        $valor = 150.25;
+        $ano_forma_pagamento = 0;
+        $codigo_forma_pagamento = 4;
+        $quantidade_parcelas = 1;
+        $principal = true;
+        $data_base_vencimento = 20/10/2019;
+
+        // Params Array
+        $paramsPay = array(
+                        'identificador_proprio'=>$identificador_proprio, 
+                        'ano_calculo'=>$ano_calculo, 
+                        'data_base_calculo'=>$data_base_calculo,
+                        'codigo_sub_receita'=>$codigo_sub_receita,
+                        'cpfcnpj'=>$cpfcnpj, 
+                        'codigo_bloco'=>$codigo_bloco, 
+                        'codigo_item'=>$codigo_item,
+                        'sequencia_item'=>$sequencia_item,
+                        'valor'=>$valor, 
+                        'ano_forma_pagamento'=>$ano_forma_pagamento, 
+                        'codigo_forma_pagamento'=>$codigo_forma_pagamento,
+                        'quantidade_parcelas'=>$quantidade_parcelas,
+                        'principal'=>$principal,
+                        'data_base_vencimento'=>$data_base_vencimento
+                     );
+        // Create the SoapClient instance
+        $url    = "https://homologacao.atende.net/atende.php?pg=services&service=WGTLancamentoTributario&cidade=brusque";        
+        echo "Testando 1";
+        $client = new SoapClient($url, $params);
+        echo "Testando 2";
+        // Call wsdl function
+        $result = $client->__soapCall("Payment", array(
+            "Payment" => array(
+                "login"        => $user,
+                "password"    => $password
+               
+            )
+        ), NULL, $paramsPay); 
+
+        // Echo the result
+        echo "<pre>".print_r($result, true)."</pre>";
+        if($result->Payment->Status == "Success")
+            {
+                echo "Boleto Gerado!";
+            }
+   
+    } catch (Exception $e) {
+        echo "<h2>Exception Error!</h2>";
+        echo $e->getMessage();
+    }
+    
+?>
