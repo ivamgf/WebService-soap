191bd133b486885adddbcf66ccda5dd472c014fb
IVAM GALVAO FILHO <programador.ivam@gmail.com>
2019-11-22 17:18:16 -0300

    Created tryBlock.php

---
+++ tryBlock.php
@@ -0,0 +1,12 @@
+<?php
+// try Block
+try {
+
+	// Function block
+
+} catch (Exception $e) {
+        echo "<h2>Exception Error!</h2>";
+        echo $e->getMessage();
+    }
+// try Block        
+?>
